89cba639db95fe3d3f972bd5c1e09a6608de27c0
Test User <test@test.test>
2023-11-15 17:46:28 +0200

    improved ball movement, adding more visuals to title screen

--- src/main/java/org/example/Ball.java
+++ src/main/java/org/example/Ball.java
@@ -5,8 +5,10 @@ import java.awt.*;
 public class Ball {
     private int ballX, ballY;
     private int diameter = 15;
-    private int xSpeed = 3;
-    private int ySpeed = 3;
+    private int xSpeed = 8;
+    private int ySpeed = 8;
+
+
 
     public Ball(int ballX, int ballY) {
         this.ballX = ballX;
@@ -24,8 +26,23 @@ public class Ball {
     }
 
     public void checkPaddleCollision(Paddle paddle) {
-        if(ballX + diameter > paddle.getPaddleXPos() && ballX < paddle.getPaddleXPos() + paddle.getWidth() && ballY + diameter > paddle.getPaddleYPos() && ballY < paddle.getPaddleYPos() + paddle.getHeight()) {
-            xSpeed = -xSpeed;
+        if (ballX + diameter > paddle.getPaddleXPos() && ballX < paddle.getPaddleXPos() + paddle.getWidth() && ballY + diameter > paddle.getPaddleYPos() && ballY < paddle.getPaddleYPos() + paddle.getHeight()) {
+            int paddleCenterY = paddle.getPaddleYPos() + paddle.getHeight() / 2;
+            int relativeBallY = ballY + diameter / 2 - paddleCenterY;
+            double normalizedRelativeY = (double) relativeBallY / ((double) paddle.getHeight() / 2);
+
+            double reflectionAngle = normalizedRelativeY * (Math.PI / 3.5);
+
+            double speed = 8;
+
+            xSpeed = (int) (speed * Math.cos(reflectionAngle));
+            ySpeed = (int) (speed * Math.sin(reflectionAngle));
+
+            if (xSpeed > 0 && ballX < paddle.getPaddleXPos() + paddle.getWidth() / 2) {
+                xSpeed = -xSpeed;
+            } else if (xSpeed < 0 && ballX > paddle.getPaddleXPos() + paddle.getWidth() / 2) {
+                xSpeed = -xSpeed;
+            }
         }
     }
 
